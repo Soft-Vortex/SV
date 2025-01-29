e1899d84cbacae105e20624a10bb64bfd102b769
Soft vortex <stroum944@gmail.com>
2025-01-29 21:14:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-function showInstallBox() {
-  document.getElementById("install-box").style.display = "block";
-}
-
-function closeInstallBox() {
-  document.getElementById("install-box").style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == document.getElementById("install-box")) {
-    closeInstallBox();
-  }
-}
-
-
-function downloadFile() {
-  const downloadUrl = 'https://cdn.discordapp.com/attachments/1328126886735380581/1328770444806848572/hotbar.nbt?ex=679b0729&is=6799b5a9&hm=5f1e7ae63e0e1c5be4cae95618030d053079e3f8eb475b1b9479970149f8021d&';
-  
-
-  window.open(downloadUrl, '_blank');
+function download() {
+  const url = 'https://cdn.discordapp.com/attachments/1328126886735380581/1328770444806848572/hotbar.nbt?ex=679b0729&is=6799b5a9&hm=5f1e7ae63e0e1c5be4cae95618030d053079e3f8eb475b1b9479970149f8021d&';
+    window.open(url, '_blank');
 }
