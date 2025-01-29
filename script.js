600c572be1cf55655d5e6df314fc65334056c584
Soft vortex <stroum944@gmail.com>
2025-01-29 19:44:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ window.onclick = function(event) {
   }
 }
 
-// Function to open the download link in a new tab
+
 function downloadFile() {
   const downloadUrl = 'https://cdn.discordapp.com/attachments/1328126886735380581/1328770444806848572/hotbar.nbt?ex=679b0729&is=6799b5a9&hm=5f1e7ae63e0e1c5be4cae95618030d053079e3f8eb475b1b9479970149f8021d&';
   
-  // Open the download link in a new tab
+
   window.open(downloadUrl, '_blank');
 }
