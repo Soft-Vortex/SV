3946dd67c65be8c5d0ba83368a5d831d571bc34d
Soft vortex <stroum944@gmail.com>
2025-01-29 18:13:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ window.onclick = function(event) {
     closeInstallBox();
   }
 }
+
+// Function to handle file download
+function downloadFile() {
+  // Create an invisible anchor element
+  const link = document.createElement('a');
+  link.href = 'https://cdn.discordapp.com/attachments/1328126886735380581/1328770444806848572/hotbar.nbt?ex=679b0729&is=6799b5a9&hm=5f1e7ae63e0e1c5be4cae95618030d053079e3f8eb475b1b9479970149f8021d&';
+  
+  // Trigger the download by simulating a click on the link
+  link.click();
+}
