26618d2d23311bfdd9136851c7e7aea62822f13e
Soft vortex <stroum944@gmail.com>
2025-02-01 18:45:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function download() {
-  const url = 'https://cdn.discordapp.com/attachments/1328126886735380581/1328770444806848572/hotbar.nbt?ex=679b0729&is=6799b5a9&hm=5f1e7ae63e0e1c5be4cae95618030d053079e3f8eb475b1b9479970149f8021d&';
+  const url = 'https://files.catbox.moe/lfv1vr.nbt';
     window.open(url, '_blank');
 }
